491bb37e6a88d917a8ab47d95c1bcb3ec314f4db
aleksgav <alex.phyp@gmail.com>
2020-04-14 01:55:37 +0300

    feat: slime & bootstrap & terms & l10n

--- mix.exs
+++ mix.exs
@@ -42,6 +42,7 @@ defmodule Cookpod.MixProject do
       {:ecto_sql, "~> 3.1"},
       {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 2.11"},
+      {:phoenix_slime, "~> 0.13.1"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
